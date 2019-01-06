e561b0242acebd027406e0abe77b902b46b98dff
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-01-06 02:07:55 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,7 @@
 def oxford_comma(array)
     if array.length != 1
       array[-1] = "and #{array[-1]}"
-    end
-    if array.length > 2
+    elsif array.length > 2
       return array.join(", ")
     else
      return array.join(" ")
