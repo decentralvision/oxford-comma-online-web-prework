dd4ac5e03ab4ec6ba42057970620464a4f9171de
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-01-06 02:09:44 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,8 @@
 def oxford_comma(array)
     if array.length > 1
       array[-1] = "and #{array[-1]}"
-    end
-    if array.length > 2
+      return array.join(" ")
+    elsif array.length > 2
       return array.join(", ")
     else
      return array.join(" ")
